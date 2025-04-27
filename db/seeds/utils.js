d23e448d98966b7408add35f8e58be306a17c4e7
Kaki LAI <kakikakikakiuk@gmail.com>
2025-04-27 22:49:36 +0100

    complete seed

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,18 +1,29 @@
 const db = require("../../db/connection");
+const { articleData } = require("../data/test-data");
 
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   if (!created_at) return { ...otherProperties };
   return { created_at: new Date(created_at), ...otherProperties };
 };
 
-exports.createRef = (articlesData) => {
-  if(articlesData.length === 0){
-  return {}
-}
-  
-const result = {}
-result[articlesData[0].title]=articlesData[0].article_id
-return result
-}
+// exports.createRef = (articlesData) => {
+//   if (articlesData.length === 0) {
+//     return {};
+//   }
 
+//   const result = {};
+//   result[articlesData[0].title] = articlesData[0].article_id;
+//   return result;
+// };
 
+exports.createArticlesLookupObj = (articlesData) => {
+  if (articlesData.length === 0) {
+    return {};
+  }
+
+  const lookupObj = {};
+  articlesData.forEach((article) => {
+    lookupObj[article.title] = article.article_id;
+  });
+  return lookupObj;
+};
